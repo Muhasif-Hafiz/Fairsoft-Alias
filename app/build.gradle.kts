e59cb21e30ab74611823334d4416d6520c14573a
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2025-06-25 12:32:23 +0530

    App Actions added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,16 +13,11 @@ android {
         applicationId = "com.muhasib.aliascreation"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        
-
     }
-
-
     buildTypes {
         release {
             isMinifyEnabled = false
