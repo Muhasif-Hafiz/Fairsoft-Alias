84056325d03ceac7e711482257afe0e5d484ba3f
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2025-06-28 14:56:59 +0530

    Shortcut Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.muhasib.aliascreation"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/muhasib/aliascreation/activity/CenterActivity.kt
+++ app/src/main/java/com/muhasib/aliascreation/activity/CenterActivity.kt
@@ -1,8 +1,12 @@
 package com.muhasib.aliascreation.activity
 
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.ShortcutManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +17,7 @@ import com.muhasib.aliascreation.databinding.ActivityCenterBinding
 class CenterActivity : AppCompatActivity() {
 
     private lateinit var  binding : ActivityCenterBinding
+    @SuppressLint("WrongThread")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +27,10 @@ class CenterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            val manager = getSystemService(ShortcutManager::class.java)
+            Log.d("SHORTCUTS", "Shortcuts: " + manager.dynamicShortcuts.size)
         }
         binding.MainActivity.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
