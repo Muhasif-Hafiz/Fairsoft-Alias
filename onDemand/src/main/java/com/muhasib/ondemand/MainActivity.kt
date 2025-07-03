3ae8ccaa1fdeb36eb2126b54bab78e23d543b9d9
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2025-07-03 16:19:58 +0530

    Dynamic Feature Module Added

---
+++ onDemand/src/main/java/com/muhasib/ondemand/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.muhasib.ondemand
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.play.core.splitcompat.SplitCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_on_demand)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+
+        SplitCompat.install(this)
+    }
+}
